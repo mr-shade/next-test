9e4d1374c6a6417a09da2d308a021676ad53734c
SH20RAJ <shaswatraj3@gmail.com>
2025-05-12 16:09:06 +0530

    Add MongoDB integration with Prisma and implement Todo app functionality

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 setupDevPlatform().catch(console.error);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configure environment variables to be available in the browser
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  }
+};
 
 export default nextConfig;
